44bcede4dc87ca9ef96478e4a5c116d6df6b1ec2
KevinCaires <kevincaires@ymail.com>
2021-03-15 21:50:19 -0300

    ADD modulo de instalação.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(
+    name='dns_enum',
+    verision='0.0.1',
+    description='Enumera DNS.',
+    author='Kevin Caires',
+    scripts=['dns_enum'],
+)
